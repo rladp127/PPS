4db6b0a849ac7168d9313486da55bf5ca788ab23
rladp127 <rladp127@naver.com>
2023-07-23 17:15:55 +0900

    2week

--- PPS/김예인_2Week/A015.cpp
+++ PPS/김예인_2Week/A015.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // 처음 5자리의 숫자들이 04256이면, 각 숫자를 제곱한 수들의 합 0+16+4+25+36 = 81 을 10으로 나눈 나머지인 1이 검증수이다.
 
-int main () {
+int main () { 
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
 
